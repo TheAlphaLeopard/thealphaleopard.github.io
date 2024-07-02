4df47ca523a930d938d356e6c8ef8d38e8a224ad
TheAlphaLeopard <148156503+TheAlphaLeopard@users.noreply.github.com>
2024-07-02 21:43:06 +0000

    cooler menu like googles

--- home/script.js
+++ home/script.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
+    const overlay = document.getElementById('overlay');
     menu.classList.toggle('open');
+    overlay.classList.toggle('open');
 }
